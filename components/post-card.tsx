da589533e3ed34e685c23c909b8f1ab3e9350bdb
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-01 01:21:45 +0700

    add blur image animation

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -1,23 +1,29 @@
+'use client';
 import { IPost } from '@/types';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface IPostCard {
    post: IPost;
 }
 
 const PostCard = ({ post }: IPostCard) => {
+   const [isImageReady, setIsImageReady] = useState(false);
+
    return (
       <Link href={`/${post.slug}`} passHref>
          <div className='flex flex-col group rounded-md border overflow-hidden bg-white'>
-            <div className='h-[250px]'>
+            <div className='h-[250px] w-full overflow-hidden'>
                <Image
                   src={post.thumbnailUrl}
-                  width={500}
-                  height={500}
+                  width={300}
+                  height={300}
                   alt={post.title}
-                  className='w-full h-full object-cover object-center group-hover:brightness-90 transition-all blur duration-[2s] border-b'
-                  onLoadingComplete={(image) => image.classList.remove('blur')}
+                  className={`w-full h-full object-cover object-center group-hover:brightness-90 transition-all duration-[1s] border-b ${
+                     isImageReady ? 'blur-0' : 'blur-3xl'
+                  }`}
+                  onLoad={() => setIsImageReady(true)}
                />
             </div>
             <div className='flex flex-col gap-2 p-3 max-w-[700px]'>
