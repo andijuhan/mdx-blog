e05543b38fb32f956e4da904c3380a419dde9987
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-02 10:53:38 +0700

    fix single post page

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -56,14 +56,15 @@ const page = async ({ params }: { params: { slug: string } }) => {
    const { data, mdxSource } = await getContent(params.slug);
 
    return (
-      <div>
+      <div className='w-full'>
          <FeaturedImagePost data={data} />
          <h1 className='text-3xl font-bold mb-3'>{data.title}</h1>
-         <div className='mb-4 flex gap-4 border-b dark:border-b-gray-700 pb-2'>
-            <div>
-               <span className='text-gray-500'>{data.date}</span>
+         <div className='mb-4 w-full flex overflow-x-scroll scrollbar-thin gap-4 border-b dark:border-b-gray-700 pb-2'>
+            <div className='min-w-max'>
+               📅 <span className='text-gray-500'>{data.date}</span>
             </div>
-            <div>
+            <div className='min-w-max'>
+               <span className='mr-2'>🏷</span>
                {data.tags.map((tag, index) => (
                   <Link href={`/tag/${tag}`} key={index} passHref>
                      <span className='text-indigo-500 dark:text-indigo-400 hover:text-indigo-700 mr-2'>
