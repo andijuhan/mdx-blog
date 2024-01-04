150842bc7ce3e01e9edd7d8f3a5c6e7a2b336692
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-04 17:16:47 +0700

    update

--- actions/getPosts.ts
+++ actions/getPosts.ts
@@ -64,7 +64,7 @@ const getPosts = async ({
 
    const postsLimited = sortedPostByDate.slice(startIndex, endIndex); // Mengambil Limited
 
-   await new Promise((resolve) => setTimeout(resolve, 500));
+   //await new Promise((resolve) => setTimeout(resolve, 500));
    return { posts: postsLimited, totalCount };
 };
 
