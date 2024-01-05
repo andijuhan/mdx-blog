562f749647ed8cad2ae9dc6e0a9796b8ded59946
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-05 21:38:34 +0700

    update

--- components/post-card.tsx
+++ components/post-card.tsx
@@ -14,21 +14,21 @@ const PostCard = ({ post }: IPostCard) => {
    return (
       <Link href={`/${post.slug}`} passHref>
          <div className='flex flex-col group rounded-md border dark:border-gray-700 overflow-hidden bg-white dark:bg-gray-700'>
-            <div className='h-[300px] md:h-[250px] lg:h-[200px] w-full overflow-hidden'>
+            <div className='h-[300px] md:h-[250px] lg:h-[200px] w-full overflow-hidden px-3 pt-3 rounded'>
                <Image
                   src={post.thumbnailUrl}
                   width={300}
                   height={300}
                   quality={75}
                   alt={post.title}
-                  className={`w-full h-full object-cover object-center group-hover:brightness-90 transition-all duration-[2s] border-b dark:border-gray-700 ${
+                  className={`w-full h-full object-cover rounded object-center group-hover:brightness-90 transition-all duration-[2s] border dark:border-gray-700 ${
                      isImageReady ? 'blur-0' : 'blur-3xl'
                   }`}
                   onLoad={() => setIsImageReady(true)}
                />
             </div>
             <div className='flex flex-col gap-2 p-3 max-w-[700px]'>
-               <h2 className='text-lg font-semibold tracking-tight text-gray-800 dark:text-gray-200 group-hover:text-indigo-500 dark:group-hover:text-indigo-400 transition-colors duration-300 line-clamp-1 lg:line-clamp-2'>
+               <h2 className='text-xl md:text-lg font-semibold tracking-tight text-gray-800 dark:text-gray-200 group-hover:text-indigo-500 dark:group-hover:text-indigo-400 transition-colors duration-300 line-clamp-2'>
                   {post.title}
                </h2>
                <span className='text-gray-700 dark:text-gray-300'>
