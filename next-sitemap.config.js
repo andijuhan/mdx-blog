f33a762a5b99db0aae3d44223c072f6c0187949d
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-13 21:15:11 +0700

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.BASE_URL || "https://blog.juhancoding.dev",
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+};
