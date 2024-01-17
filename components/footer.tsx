a38213289270c6f6a44c12e60468a08902c6d9d9
andijuhan <80870917+andijuhan@users.noreply.github.com>
2024-01-17 23:03:03 +0700

    fix nav & footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,8 +4,8 @@ const Footer = () => {
   return (
     <div className="mt-5 w-full border-t bg-gray-50 shadow-lg dark:border-t-gray-800 dark:bg-gray-900">
       <div className="mx-auto max-w-5xl p-5 text-center text-gray-700 dark:text-gray-200">
-        &copy; {new Date().getFullYear()} {blogSetting.title}. All rights
-        reserved
+        &copy; {new Date().getFullYear()} blog.juhancoding.dev. Build with
+        Nextjs & Tailwindcss. All reserved
       </div>
     </div>
   );
--- components/nav.tsx
+++ components/nav.tsx
@@ -9,12 +9,12 @@ const Nav = () => {
     <nav className="fixed left-0 top-0 z-50 w-full bg-black shadow-lg">
       <div className="mx-auto flex max-w-4xl items-center px-3 py-4 ">
         <Link href={"/"} passHref>
-          <h1 className="mr-10 text-xl font-bold text-indigo-400">
+          <h1 className="mr-10 text-xl font-bold text-indigo-400 lg:text-2xl">
             👨🏻‍💻{blogSetting.title}
           </h1>
         </Link>
         {/* menu */}
-        <div className="hidden items-center gap-7 lg:flex">
+        <div className="hidden items-center gap-7 ">
           {menus.map((menu, index) => (
             <Link href={menu.path} key={index} passHref>
               <span className="text-gray-100 hover:text-indigo-300 dark:text-gray-300">
